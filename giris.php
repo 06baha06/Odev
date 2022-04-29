3324968d0208fd9e3c9928980033c82991d23de9
06baha06 <06baha06@gmail.com>
2022-04-30 01:24:44 +0300

    php

---
+++ giris.php
@@ -0,0 +1,20 @@
+<section class="p-5 text-center iletisim">
+    <div class="container">
+
+
+       <?php 
+            if (isset($_POST['ad'], $_POST['şifre'] )
+            && ($_POST['ad']=='b211210049@sakarya.edu.tr')
+            && ($_POST['şifre']=='b211210049')) {
+            echo "Hoşgeldiniz ".$_POST['ad'] ;
+            
+            }else{
+            echo"Bilgilerinizi Kontrol Edip Tekrar Giriş Yapın.<br>";
+            
+            }
+            echo "<p> <a href='giriş.html'>&lt;GERİ DÖN&gt;</a></p> ";
+       ?>
+    </div>
+
+
+</section>
